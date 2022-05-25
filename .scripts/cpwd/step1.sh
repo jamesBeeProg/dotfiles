22a41067185b18437c1eb53ba40a00b6d1639414
JamesBeeProg <JamesBeeProg@gmail.com>
2022-05-25 13:40:48 -0400

    Add cpwd scripts

---
+++ .scripts/cpwd/step1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Select disk
+lsblk
+read -e -p "Enter disk path: " DRIVE
+
+# Partiton the disk
+parted $DRIVE --script mklabel gpt
+parted $DRIVE --script mkpart ESP fat32 1MiB 513MiB
+parted $DRIVE --script set 1 boot on
+parted $DRIVE --script mkpart primary ext4 513MiB 100%
+
+# Format the parititons
+mkfs.fat -F32 "${DRIVE}1"
+mkfs.ext4 "${DRIVE}2"
+
+# Mount them
+mount "${DRIVE}2" /mnt
+mkdir /mnt/boot
+mount "${DRIVE}1" /mnt/boot
+
+# Install base and kernal packages
+pacstrap /mnt base linux linux-firmware base-devel
+
+# Save mount layout
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Change into the computer
+cp ./step2.sh /mnt
+arch-chroot /mnt /step2.sh
---
+++ .scripts/cpwd/step2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Set hostname
+echo cpwdtoast > /etc/hostname
+
+# Setup boot loader
+pacman -S grub efibootmgr intel-ucode
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Setup network
+pacman -S networkmanager
+systemctl enable NetworkManager.service
+
+# Setup locale
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+
+# Setup time
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc --utc
+
+# Setup sudo
+echo 'ALL ALL=(ALL:ALL) NOPASSWD: ALL' | EDITOR='tee -a' visudo
+
+# Clean up
+rm /step2.sh
+exit
